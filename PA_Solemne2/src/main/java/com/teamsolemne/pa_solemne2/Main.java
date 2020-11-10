a7e35a4a4277a8b48801deb35ab7c4463dd69f9e
Matías Rivera Maldonado <matias.rivera@mrivem.com>
2020-11-10 12:05:37 -0300

    Cree el proyecto con clase inicial

---
+++ PA_Solemne2/src/main/java/com/teamsolemne/pa_solemne2/Main.java
@@ -0,0 +1,8 @@
+package com.teamsolemne.pa_solemne2;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("PA Solemne 2");
+        System.out.println("Integrantes: Javier Hernández D - Matías Rivera M");
+    }
+}
